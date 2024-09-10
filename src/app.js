9692484f02c2e37055b15161cc37329fb2ab0ee5
Michaelrick88 <michaelrick88@hotmail.com>
2024-09-10 17:16:44 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let nombres = ["el perro", "el gato", "el cartero", "el pez"];
+  let verbos = ["se canso", "se durmio", "se estrello", "se salio"];
+  let predicado = [
+    "en el parque",
+    "en el hospital",
+    "en la casa",
+    "en la escuela"
+  ];
+
+  let nombresIndice = Math.floor(Math.random() * nombres.length);
+  let verbosIndice = Math.floor(Math.random() * verbos.length);
+  let predicadoIndice = Math.floor(Math.random() * predicado.length);
+
+  document.querySelector("#excusa").innerHTML =
+    nombres[nombresIndice] +
+    " " +
+    verbos[verbosIndice] +
+    " " +
+    predicado[predicadoIndice];
+
   console.log("Hello Rigo from the console!");
 };
